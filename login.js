ed4c8ccfb3586991a44dd8c56796883b2755039a
Gustavo Labos <gustavo.labos@portalsesisp.org.br>
2023-05-16 14:50:38 -0300

    d

--- login.js
+++ login.js
@@ -1,14 +1,36 @@
 const nome = document.getElementById("inome");
 const senha = document.getElementById("senha");
 const csenha = document.getElementById("csenha");
+const formulario = document.getElementById("formulario")
+const mensagem = document.querySelector(".mensagem")
 
-
-formulario.onsubmit = (evento) =>{
-if (nome.value == ""){
+formulario.onsubmit = (evento) => {
+    if (nome.value == ""){
     evento.preventDefault();
-    mensagem.innerHTML= "<p> Digite seu nome </p>";
+    mensagem.innerHTML = " Nome incorreto";
     nome.focus();
-    return null;
+    return null;}
+
+    if (senha.value == ""){
+    evento.preventDefault();
+    mensagem.innerHTML = " Senha Incorreta";
+    senha.focus();
+    return null;}
+
+    if (csenha.value != senha.value){
+        evento.preventDefault();
+        mensagem.innerHTML = " Senhas não se coencidem";
+        csenha.focus();
+        return null;}
+
+    dados.forEach((elemento) => {
+        if (elemento.csenha == senha.value && elemento.senha == senha.value) {
+            evento.preventDefault();
+            mensagem.innerHTML="Logado";
+            return true;
+        } else {
+            mensagem.innerHTML ="Senha ou E-mail Incorreto"
+        }
+    });
 }
 
-}
